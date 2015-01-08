95fa769b2c5424d41c1193651f25005bed60722a
Achyut Reddy <achyutreddy24@gmail.com>
2015-01-07 20:09:14 -0500

    Add check for incorrect pattern

--- Class_Pattern.py
+++ Class_Pattern.py
@@ -1,5 +1,12 @@
 class Pattern(object):
 
-    def __init__(self):
+    def __init__(self, pattern):
         self.consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
         self.vowels = ['a','e','i','o','u']
+        self.pattern = pattern
+        
+        for letter in self.pattern:
+            if letter != 'C' and letter != 'V':
+                raise TypeError("Error: pattern is incorrectly formatted\nHere is and example pattern 'CVCVC'")
+                
+x = Pattern('CVCV')

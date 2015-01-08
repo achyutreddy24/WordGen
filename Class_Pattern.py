6cf147986317ed5c7d7fe4fa9785f2d88f7d7e8a
Achyut Reddy <achyutreddy24@gmail.com>
2015-01-07 19:58:03 -0500

    Add vowels and consonants constants

---
+++ Class_Pattern.py
@@ -0,0 +1,5 @@
+class Pattern(object):
+
+    def __init__(self):
+        self.consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+        self.vowels = ['a','e','i','o','u']
